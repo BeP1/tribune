f44cce50c48e6d29a4852aac2278ed4973a45c28
BeP1 <artemneigsal@gmail.com>
2024-12-12 14:37:47 +0200

    lab2

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  {ignores: ["server.js", "src/App.test.js"] },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
