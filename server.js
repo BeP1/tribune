29629c7859b788c8c6647c34cf33695d554a4bbd
BeP1 <artemneigsal@gmail.com>
2024-12-19 21:29:42 +0200

    Edit package.json

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+
 
 const LEAGUES = [
   39, // EPL
