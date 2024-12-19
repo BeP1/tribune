f9447732cca7ecbd02e3a6c638a648624f08f573
BeP1 <artemneigsal@gmail.com>
2024-12-20 01:03:57 +0200

    configure routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MatchDetails from './components/MatchDetails';
 import Home from './components/Home';
 import ClubDetails from './components/ClubDetails';
@@ -7,14 +7,16 @@ import Player from './components/Player';
 
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/match/:id" element={<MatchDetails />} />
-        <Route path="/club/:club_id/:league_id" element={<ClubDetails />} />
-        <Route path="/player/:id" element={<Player />} />
-      </Routes>
-    </div>
+    <BrowserRouter basename="/tribume">
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/match/:id" element={<MatchDetails />} />
+          <Route path="/club/:club_id/:league_id" element={<ClubDetails />} />
+          <Route path="/player/:id" element={<Player />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
